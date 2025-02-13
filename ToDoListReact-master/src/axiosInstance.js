d1d8e786fc575a9a9edd388966cb214b7a94ab83
“Dvora” <dvoraloifer@gmail.com>
2025-02-14 01:41:50 +0200

    “Update”

--- ToDoListReact-master/src/axiosInstance.js
+++ ToDoListReact-master/src/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5250', // החלף בכתובת ה-URL של ה-API שלך
+    // baseURL: 'http://localhost:5250', // החלף בכתובת ה-URL של ה-API שלך
+    baseURL:process.env.REACT_APP_API_URL
+    // ,
+
 });
 
 // הוספת אינטרצפטור לטיפול בשגיאות 401
--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5250';
+// axios.defaults.baseURL = 'http://localhost:5250';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 // הגדרת אינטרספטורים
 axios.interceptors.request.use(
