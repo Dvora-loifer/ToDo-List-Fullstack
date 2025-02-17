4fedc5df1ce8bc6e98734a8ccda7fb117ffcc679
“Dvora” <dvoraloifer@gmail.com>
2025-02-17 18:30:07 +0200

    “Try4”

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -33,7 +33,7 @@ public partial class ToDoDbContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("items_tbl");
+            entity.ToTable("Items");
 
             entity.Property(e => e.Name).HasMaxLength(100);
         });
